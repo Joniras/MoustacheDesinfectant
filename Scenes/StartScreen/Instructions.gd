c6286ffc19de7686a4f6aec5244444c593622008
mamanigrasso <m.manigrasso@outlook.com>
2020-04-26 14:43:49 +0200

    Added click sound to back button

--- Scenes/StartScreen/Instructions.gd
+++ Scenes/StartScreen/Instructions.gd
@@ -12,8 +12,10 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("Left_Mouse"):
+		Sound.get_node("AudioStreamPlayerClick").play()
+	pass
 
 
 func _on_button_back_pressed():
